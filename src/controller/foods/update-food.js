0c2740f2bf4101e77060f5fca00dc70b3301bf84
Tuvshee555 <tuvshee894@gmail.com>
2025-03-15 15:50:44 +0800

    ss

--- src/controller/foods/update-food.js
+++ src/controller/foods/update-food.js
@@ -1,6 +1,6 @@
 import { FoodModel } from "../../modules/food.model.js";
 export const updateFood = async (req, res) => {
-  const { _id, foodName, price, image, ingredients } = req.body;
+  const { _id, foodName, price, image, ingredients, category } = req.body;
   try {
     const updatedFood = await FoodModel.findByIdAndUpdate(
       { _id: _id },
@@ -9,9 +9,11 @@ export const updateFood = async (req, res) => {
         price: price,
         image: image,
         ingredients: ingredients,
+        category: category
       }
     );
     res.status(202).send(updatedFood);
+    
   } catch (error) {
     res.status(500).send(`Error while updating food ${error}`);
   }
