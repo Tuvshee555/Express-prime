c4d04b0cfdd02feaf97dbd9b0cba4e0d6aa599cd
Түвшинсайхан <tuvshee894@gmail.com>
2025-10-18 20:50:35 +0800

    why

--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -1,36 +1,3 @@
-// import express from "express";
-// import { getUsers } from "../controller/users/get-users.js";
-// import { deleteUser } from "../controller/users/delete-user.js";
-// import { updateUser } from "../controller/users/update-user.js";
-// import { createUser } from "../controller/users/create-user.js";
-// import { loginUser } from "../controller/users/login-user.js";
-// import { validateUserId } from "../middleware/users/validate-user-id.js";
-// import { forgotPassword } from "../controller/users/forgot-password.js";
-// import { resetPassword } from "../controller/users/reset-password.js";
-// import { googleAuth } from "../controller/users/google-auth.js";
-
-// export const userRouter = express.Router();
-
-// // Example for src/components/PostUser.tsx
-// const handleGoogleLogin = async (credentialResponse) => {
-//   await axios.post("http://localhost:4000/user/auth/google", {
-//     token: credentialResponse.credential,
-//   });
-//   // handle response, redirect, etc.
-// };
-
-// userRouter.post("/forgot-password", forgotPassword);
-// userRouter.post("/auth/google", googleAuth);
-// userRouter.post("/reset-password", resetPassword);
-// userRouter.get("/", getUsers);
-// userRouter.delete("/:id", validateUserId, deleteUser);
-// userRouter.put("/:id", validateUserId, updateUser);
-// userRouter.post("/", createUser);
-// userRouter.post("/login", loginUser);
-// userRouter.post("/google-auth", googleAuth);
-
-
-
 import express from "express";
 import { getUsers } from "../controller/users/get-users.js";
 import { deleteUser } from "../controller/users/delete-user.js";
